1c83fcd16d85853ad198c5968effd2dd6e20a7b6
stanislav.pastushenko <stas8327634@gmail.com>
2023-01-04 12:01:59 +0200

    feat: add routes lazy loading

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,57 +1,13 @@
 import * as React from 'react';
 import { useRoutes } from 'react-router-dom';
-import { AUTH_ROUTES, VIDEO_ROUTES } from 'shared/config/routes';
+import { VIDEO_ROUTES } from 'shared/config/routes';
 import { AuthGuard } from 'app/guards/auth-guard';
-import { LoginPage } from 'modules/auth/pages/login/login';
-import { RegisterPage } from 'modules/auth/pages/register/register';
-import { ForgotPasswordPage } from 'modules/auth/pages/forgot-password/forgot-password';
-import { AuthPage } from 'modules/auth/pages/auth';
-import { NewPasswordPage } from 'modules/auth/pages/new-password/new-password';
-import { SetUpProfilePage } from 'modules/auth/pages/set-up-profile/set-up-profile';
-import { VerifyEmailPage } from 'modules/auth/pages/verify-email/verify-email';
 import { HomePage } from 'modules/videos/pages/home';
-import { MustVerifyEmailPage } from 'modules/auth/pages/must-verify-email/must-verify-email';
+import { authRoutes } from 'app/routes/auth-routes';
 
 export const AppRoutes: React.FC = () => {
 	const routes = useRoutes([
-		{
-			path: AUTH_ROUTES.ROOT.path,
-			element: (
-				<AuthGuard>
-					<AuthPage />
-				</AuthGuard>
-			),
-			children: [
-				{
-					path: AUTH_ROUTES.LOGIN.path,
-					element: <LoginPage />,
-				},
-				{
-					path: AUTH_ROUTES.REGISTER.path,
-					element: <RegisterPage />,
-				},
-				{
-					path: AUTH_ROUTES.FORGOT_PASSWORD.path,
-					element: <ForgotPasswordPage />,
-				},
-				{
-					path: AUTH_ROUTES.NEW_PASSWORD.path,
-					element: <NewPasswordPage />,
-				},
-				{
-					path: AUTH_ROUTES.SET_UP_PROFILE.path,
-					element: <SetUpProfilePage />,
-				},
-				{
-					path: AUTH_ROUTES.VERIFY_EMAIL.path,
-					element: <VerifyEmailPage />,
-				},
-				{
-					path: AUTH_ROUTES.MUST_VERIFY_EMAIL.path,
-					element: <MustVerifyEmailPage />,
-				},
-			],
-		},
+		...authRoutes,
 
 		{
 			path: VIDEO_ROUTES.MY_VIDEOS.path,
