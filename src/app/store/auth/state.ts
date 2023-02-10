2abed2612633841d92d2920e43853118d845dd29
stanislav.pastushenko <stas8327634@gmail.com>
2023-02-10 14:32:13 +0200

    refactor: replace proxyWithComputed with proxy

--- src/app/store/auth/state.ts
+++ src/app/store/auth/state.ts
@@ -1,4 +1,5 @@
-import { devtools, proxyWithComputed } from 'valtio/utils';
+import { proxy } from 'valtio';
+import { devtools } from 'valtio/utils';
 import { IToken } from 'shared/types/token';
 import jwtDecode from 'jwt-decode';
 import { LocalStorageService } from 'shared/services/local-storage-service';
@@ -6,32 +7,27 @@ import { LocalStorageService } from 'shared/services/local-storage-service';
 interface IAuthState {
 	user: IToken | null;
 	setUser: (token: string | null) => void;
-}
-
-interface IAuthStateComputed {
 	isLoggedIn: boolean;
 }
 
-export const authState = proxyWithComputed<IAuthState, IAuthStateComputed>(
-	{
-		user: null,
-		setUser(token: string | null) {
-			if (!token) {
-				LocalStorageService.remove('token');
-				this.user = null;
+export const authState = proxy<IAuthState>({
+	user: null,
+	setUser(token: string | null) {
+		if (!token) {
+			LocalStorageService.remove('token');
+			this.user = null;
 
-				return;
-			}
+			return;
+		}
 
-			const userData: IToken = jwtDecode(token);
-			LocalStorageService.set('token', token);
+		const userData: IToken = jwtDecode(token);
+		LocalStorageService.set('token', token);
 
-			this.user = userData;
-		},
+		this.user = userData;
 	},
-	{
-		isLoggedIn: (state) => !!state.user,
+	get isLoggedIn() {
+		return !!this.user;
 	},
-);
+});
 
 devtools(authState, { name: 'authState', enabled: true });
