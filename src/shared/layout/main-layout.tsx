4751d6d91f0ddd1c0ca000fa8465319898be8cf2
fd-mailden <fd.mailden@gmail.com>
2023-02-14 16:37:03 +0200

    refactor: main layout

--- src/shared/layout/main-layout.tsx
+++ src/shared/layout/main-layout.tsx
@@ -10,12 +10,15 @@ import {useControlVideo} from "../../modules/videos/hooks/use-record-video";
 const MainLayoutContainer = styled(Container)`
 	display: flex;
 	height: 100vh;
+	overflow: hidden;
 `;
 
 const Main = styled('main')`
 	flex-grow: 1;
 	padding: 24px 32px;
+	overflow-y: auto;
 `;
+
 
 export const MainLayout: React.FC<React.PropsWithChildren> = ({children}) => {
 	const {models: {recordWidget}, commands: {startRecording}} = useControlVideo()
