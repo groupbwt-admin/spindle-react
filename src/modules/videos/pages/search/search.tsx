08d9d6ddefa595a4493b9328b2c2b2437fc3e80b
Angelina <smmlinchik@gmail.com>
2023-03-01 17:18:13 +0200

    feat: move search logic to hook

--- src/modules/videos/pages/search/search.tsx
+++ src/modules/videos/pages/search/search.tsx
@@ -1,21 +1,11 @@
-import React, { useMemo, useState } from 'react';
-import { Link, useLocation, useSearchParams } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from '@emotion/styled/macro';
-import queryString from 'query-string';
+import { useSearch } from 'modules/videos/pages/search/use-search';
 
 import { Chip, Stack } from '@mui/material';
 
-import { ITag } from 'shared/types/video';
-
-import {
-	SearchParamsDto,
-	SearchResponseDto,
-	VideoApi,
-} from 'app/api/video-api/video-api';
-
 import { USER_ROUTES } from 'shared/config/routes';
-import { useEffectAfterMount } from 'shared/hooks/use-effect-after-mount';
-import { useFilterRequest } from 'shared/hooks/use-filter-request';
 import { getUserAvatarURL } from 'shared/utils/get-file-url';
 
 import { Avatar } from 'shared/components/avatar/avatar';
@@ -122,205 +112,43 @@ const VideoListContainer = styled.div`
 `;
 
 export const SearchPage = (props) => {
-	const location = useLocation();
-	const params: { search?: string; criteriaTags?: string } = queryString.parse(
-		location.search,
-	);
-	const [, setSearchParams] = useSearchParams();
-	const [tags, setTags] = useState<ITag[]>(() => {
-		if (!params?.criteriaTags) return [];
-		const [id, tagName] = params.criteriaTags.split('_');
-		return [{ id: id, tag: tagName }];
-	});
-	const [searchByTag, setSearchByTag] = useState(!!tags.length);
-	const [query, setQuery] = useState(params.search || '');
-	const [meta] = useState({
-		tags: { page: 1, take: 50 },
-		users: { page: 1, take: 50 },
-		videos: { page: 1, take: 50 },
-	});
-
-	const {
-		data: searchResults,
-		searchData,
-		isInitialLoading,
-		isSearching,
-	} = useFilterRequest<SearchResponseDto, SearchParamsDto, SearchParamsDto>({
-		request: (params) => {
-			if (searchByTag)
-				return VideoApi.getVideos({
-					search: query,
-					criteriaTags: tags.map((tag) => tag.id),
-					...params,
-				});
-			return VideoApi.search({ search: query, ...params });
-		},
-		searchRequest: ({ searchByTag, ...params }) => {
-			if (searchByTag) return VideoApi.getVideos(params);
-			return VideoApi.search({ ...params, meta });
-		},
-	});
-
-	useEffectAfterMount(() => {
-		if (searchByTag) return;
-		searchData(
-			() => ({ search: query }),
-			() => {
-				setSearchParams(() => {
-					return queryString.stringify(
-						{
-							search: query,
-						},
-						{ skipNull: true, skipEmptyString: true },
-					);
-				});
-			},
-		);
-	}, [query]);
-
-	const handleSearchInputChange = (value) => {
-		setQuery(value);
-		searchData(
-			() => ({
-				searchByTag: true,
-				criteriaTags: tags.map((tag) => tag.id),
-				search: value,
-			}),
-			() => {
-				setSearchParams(() => {
-					return queryString.stringify(
-						{
-							criteriaTags: tags.map((tag) => `${tag.id}_${tag.tag}`),
-							search: value,
-						},
-						{ skipNull: true, skipEmptyString: true },
-					);
-				});
-			},
-		);
-	};
-
-	const handleSearchByTag = (tag) => {
-		setSearchByTag(true);
-		setTags(() => [tag]);
-		searchData(
-			() => ({ searchByTag: true, criteriaTags: [tag.id] }),
-			() => {
-				setSearchParams(() => {
-					return queryString.stringify(
-						{
-							criteriaTags: [`${tag.id}_${tag.tag}`],
-						},
-						{ skipNull: true, skipEmptyString: true },
-					);
-				});
-			},
-		);
-		setQuery('');
-	};
-
-	const handleDeleteTag = () => {
-		setTags([]);
-		setSearchByTag(false);
-		searchData(
-			() => ({ search: query }),
-			() => {
-				setSearchParams(() => {
-					return queryString.stringify(
-						{
-							search: query,
-						},
-						{ skipNull: true, skipEmptyString: true },
-					);
-				});
-			},
-		);
-	};
-
-	const handleClear = () => {
-		setSearchByTag(false);
-		setTags([]);
-		setQuery('');
-		searchData(
-			() => ({ search: query }),
-			() => {
-				setSearchParams(() => {
-					return queryString.stringify(
-						{
-							search: query,
-						},
-						{ skipNull: true, skipEmptyString: true },
-					);
-				});
-			},
-		);
-	};
-
-	const hasResults = useMemo((): {
-		tags: boolean;
-		users: boolean;
-		videos: boolean;
-		total: () => boolean;
-	} => {
-		return searchByTag
-			? {
-					videos: !!searchResults?.data?.length,
-					tags: false,
-					users: false,
-					total() {
-						return !!searchResults?.data?.length;
-					},
-			  }
-			: {
-					tags: !!searchResults?.tags?.length,
-					users: !!searchResults?.users?.length,
-					videos: !!searchResults?.videos?.length,
-					total() {
-						return this.users || this.tags || this.videos;
-					},
-			  };
-	}, [searchResults]);
-
-	const videos = useMemo(
-		() => (searchByTag ? searchResults?.data : searchResults?.videos),
-		[searchResults, searchByTag],
-	);
+	const { models, commands } = useSearch();
 
 	return (
 		<SearchPageContainer>
 			<SearchInputExtended
-				isLoading={isSearching}
-				inputValue={query}
-				selectedTags={tags.map((tag) => tag.tag)}
-				onInputChange={handleSearchInputChange}
-				onClear={handleClear}
-				onDeleteTag={handleDeleteTag}
+				isLoading={models.isSearching}
+				inputValue={models.query}
+				selectedTags={models.tags.map((tag) => tag.tag)}
+				onInputChange={commands.handleSearchInputChange}
+				onClear={commands.handleClear}
+				onDeleteTag={commands.handleDeleteTag}
 			/>
 			<SearchContentContainer>
-				<SpinnerOverlay open={isSearching || isInitialLoading} />
-				{hasResults.total() ? (
+				<SpinnerOverlay open={models.isSearching || models.isInitialLoading} />
+				{models.hasResults.total() ? (
 					<SearchResultsContainer>
 						<StyledTypography variant="h4">Search results</StyledTypography>
-						{hasResults.tags && (
+						{models.hasResults.tags && (
 							<TagsContainer>
 								<Typography variant="h4">Tags</Typography>
 								<Stack direction="row" gap={1} marginTop={3} flexWrap="wrap">
-									{searchResults?.tags.map((tag) => (
+									{models.searchResults?.tags.map((tag) => (
 										<StyledChip
 											key={tag.tag}
 											{...props}
 											label={tag.tag}
-											onClick={() => handleSearchByTag(tag)}
+											onClick={() => commands.handleSearchByTag(tag)}
 										/>
 									))}
 								</Stack>
 							</TagsContainer>
 						)}
-						{hasResults.users && (
+						{models.hasResults.users && (
 							<ProfilesContainer>
 								<Typography variant="h4">Profiles</Typography>
 								<Stack gap={2} marginTop={3}>
-									{searchResults?.users.map((user) => (
+									{models.searchResults?.users.map((user) => (
 										<StyledLink
 											to={USER_ROUTES.USER.generate(user.id as string)}
 											key={user.id}
@@ -345,11 +173,11 @@ export const SearchPage = (props) => {
 								</Stack>
 							</ProfilesContainer>
 						)}
-						{hasResults.videos && (
+						{models.hasResults.videos && (
 							<VideosContainer>
 								<Typography variant="h4">Videos</Typography>
 								<VideoListContainer>
-									{videos?.map((video) => (
+									{models.videos?.map((video) => (
 										<VideoCard
 											key={video.id}
 											video={video}
@@ -367,10 +195,10 @@ export const SearchPage = (props) => {
 						)}
 					</SearchResultsContainer>
 				) : (
-					(!isSearching || !isInitialLoading) && (
+					(!models.isSearching || !models.isInitialLoading) && (
 						<NoResultsList
 							text={`No results for ${
-								query || `#${tags[0]?.tag}`
+								models.query || `#${models.tags[0]?.tag}`
 							}. Try something else`}
 						/>
 					)
