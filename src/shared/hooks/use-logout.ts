443217269d671c4c2516aacd4ce6322f7159f9af
Angelina <smmlinchik@gmail.com>
2023-03-08 17:28:32 +0200

    feat: add clearing query cache

--- src/shared/hooks/use-logout.ts
+++ src/shared/hooks/use-logout.ts
@@ -1,10 +1,15 @@
+import { useQueryClient } from 'react-query';
+
 import { authState } from 'app/store/auth/state';
 import { userState } from 'app/store/user/state';
 
 export function useLogout() {
+	const queryClient = useQueryClient();
+
 	const logout = () => {
 		authState.setUser(null);
 		userState.setProfile(null);
+		queryClient.clear();
 	};
 
 	return {
